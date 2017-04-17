2d2afd02487bc0bc717cc8b0d6974ac30b9d8844
Levi Renz S. Ete <lete@tspi.com.ph>
2017-04-17 09:42:54 +0800

    Socket Closing and Unregister

--- src/android/TgsiWifiPlugin.java
+++ src/android/TgsiWifiPlugin.java
@@ -110,15 +110,14 @@ public class TgsiWifiPlugin extends CordovaPlugin implements WifiP2pManager.Conn
     public void onResume(boolean multitasking) {
         super.onResume(multitasking);
         LOG.d("TAG", "onResume");
-        receiver = new P2pBroadcastReceiver(manager, channel, this);
-	    cordova.getActivity().registerReceiver(receiver, intentFilter);
+        //receiver = new P2pBroadcastReceiver(manager, channel, this);
+	    //cordova.getActivity().registerReceiver(receiver, intentFilter);
     }
 
     @Override
     public void onPause(boolean multitasking) {
         super.onPause(multitasking);
-        cordova.getActivity().unregisterReceiver(receiver);
-		//cordova.getActivity().finish();
+        //cordova.getActivity().unregisterReceiver(receiver);
     }
 
     @Override
